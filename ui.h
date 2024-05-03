f424ab8b1b510f2970b0a98433e4d0a3cff849be
Jackzeu22 <elisson.pires@bb.com.br>
2024-05-03 15:05:02 -0300

    acrescimos dos passos das aulas alura

--- ui.h
+++ ui.h
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include "mapa.h"
 
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_CIAN     "\x1b[36m"
+#define ANSI_COLOR_WHITE    "\x1b[37m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
+
 char desenhoparede[4][7] = {
-	{"......" },
-	{"......" },
-	{"......" },
-	{"......" }
+	{"......"},
+	{"......"},
+	{"......"},
+	{"......"}
 };
 
 char desenhofantasma[4][7] = {
-	{" .-.  " },
-	{"| OO| " },
-	{"|   | " },
-	{"'^^^' " }
+	{" .-.  "},
+	{"| OO| "},
+	{"|   | "},
+	{"'^^^' "}
 };
 
 char desenhoheroi[4][7] = {
-	{" .--. "  },
-	{"/ _.-'"  },
-	{"\\  '-." },
-	{" '--' "  }
+	{" .--. "},
+	{"/ _.-'"},
+	{"\\  '-."},
+	{" '--' "}
 };
 
 char desenhopilula[4][7] = {
@@ -48,17 +54,25 @@ void imprimemapa(MAPA* m) {
 
 				switch(m->matriz[i][j]) {
 					case FANTASMA:
+						printf(ANSI_COLOR_WHITE);
 						imprimeparte(desenhofantasma, parte);
+						printf(ANSI_COLOR_RESET);
 						break;
 					case HEROI:
+						printf(ANSI_COLOR_YELLOW);
 						imprimeparte(desenhoheroi, parte);
+						printf(ANSI_COLOR_RESET);
 						break;
 					case PILULA:
+						printf(ANSI_COLOR_CIAN);
 						imprimeparte(desenhopilula, parte);
+						printf(ANSI_COLOR_RESET);
 						break;
 					case PAREDE_VERTICAL:
 					case PAREDE_HORIZONTAL:
+						printf(ANSI_COLOR_RED);
 						imprimeparte(desenhoparede, parte);
+						printf(ANSI_COLOR_RESET);
 						break;
 					case VAZIO:
 						imprimeparte(desenhovazio, parte);
